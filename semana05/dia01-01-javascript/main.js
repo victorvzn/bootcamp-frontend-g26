c5f7a55bdc10afb905f465e7e66987bbf4c71ac5
Victor Villazón <victorzn23@gmail.com>
2025-06-28 09:51:41 -0500

    semana05, dia01, javascript

--- semana05/dia01-01-javascript/main.js
+++ semana05/dia01-01-javascript/main.js
@@ -1,1 +1,26 @@
-console.log('Hola JS desde main.js')
+// Comentarios
+
+// console.log('Hola JS desde main.js')
+
+/*
+  Comentario
+  de
+  múltiples
+  líneas
+*/
+
+// Declarar variables (Guardan información) con let y const
+
+const pi = 3.1415 // Es obligatorio inicializarlo
+let nombre // No es obligatori inicializarlo
+
+console.log(nombre) // Undefined
+
+nombre = 1500
+console.log(nombre) // 1500
+nombre = 'Victor'
+
+console.log(pi * 3)
+console.log(nombre) // Victor
+
+// Tipos de datos
