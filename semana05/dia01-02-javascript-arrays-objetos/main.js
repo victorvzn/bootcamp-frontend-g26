8416b768051740b0fd5a080e71351c78b1867e5f
Victor Villazón <victorzn23@gmail.com>
2025-06-28 12:06:54 -0500

    semana05, dia01, javascript arrays+objetos

--- semana05/dia01-02-javascript-arrays-objetos/main.js
+++ semana05/dia01-02-javascript-arrays-objetos/main.js
@@ -35,3 +35,49 @@ console.log(listaDeValores)
 listaDeNumeros[0] = 'Hola'
 
 console.log(listaDeNumeros)
+
+// Insertar nuevos elementos en un arreglo al final
+
+const listaDeNombres = ['Victor', 'Alessandro', 'Francesca', 'Yesica']
+
+console.log(listaDeNombres)
+
+listaDeNombres.push('Javascript')
+listaDeNombres.push('CSS')
+
+console.log(listaDeNombres)
+
+// Remover elementos del final del arreglo
+
+listaDeNombres.pop()
+listaDeNombres.pop()
+
+console.log(listaDeNombres)
+
+// Insertar un elemento en una posición determinada
+
+listaDeNombres.splice(0, 0, 'Código')
+listaDeNombres.splice(3, 0, 'Tecsup')
+
+console.log(listaDeNombres)
+
+// Eliminar un elemento en un posición determinada
+
+listaDeNombres.splice(3, 1, 'hola') // Eliminamos el elemento en la posición 3 y insertamos el texto 'hola' al mismo tiempo
+
+console.log(listaDeNombres)
+
+// Obtener el tamaño de nuestro arreglo
+
+console.log(listaDeNombres.length)
+console.log('bienvenido'.length)
+
+// Obtener el último elemento el arreglo
+
+console.log(listaDeNombres[listaDeNombres.length - 1])
+console.log(listaDeNombres.at(0))
+console.log(listaDeNombres.at(-2))
+
+// DOC: https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+
+// TODO: Otras funciones: slice (Investiguen)
