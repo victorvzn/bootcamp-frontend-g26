0a7e28898c8004ce797c63f63c6570e1419806b1
Victor Villazón <victorzn23@gmail.com>
2025-06-28 12:43:44 -0500

    semana05, dia01, javascript arrays+objetos

--- semana05/dia01-02-javascript-arrays-objetos/main.js
+++ semana05/dia01-02-javascript-arrays-objetos/main.js
@@ -109,22 +109,33 @@ const resultado = languages.filter(
 
 console.log(resultado)
 
-// Método MAP, evalue un arreglo y lo modifica y hay que pasarle una función
+// const languages = ['javascript', 'php', 'python', 'C', 'c++', 'java', 'python']
 
-const nombresConTitulo = languages.map(function (language) {
-  // return 'hola'
-  return '*' + language + '*'
-})
+// Método MAP, evalue un arreglo, lo modifica y hay que pasarle una función
+
+const nombresConTitulo = languages.map(
+  function (language) {
+    // return 'hola'
+    return '*' + language + '*'
+  }
+)
 
 console.log(nombresConTitulo)
 console.log(languages)
 
 // Método SORT, nos ayuda a ordenar un arreglo de elementos. Muta el arreglo original.
 
-const ordenandoLanguages = [...languages].sort() // Mutando el arreglo original
+const ordenandoLanguages = [...languages].sort() // ORDENA ASC. Mutando el arreglo original
 
 console.log(ordenandoLanguages)
 console.log(languages)
+
+const numeros2 = [11, 2, 1, 22, 86, 99, ]
+
+console.log(numeros2.sort(function(a, b) {
+  // return a - b // ORDENA ASC.
+  return b - a // ORDENA DESC.
+}))
 
 // Método FOREACH, nos ayuda a recorrer un arreglo sin tener ningun retorno de datos
 
@@ -138,7 +149,7 @@ console.log(miArreglo)
 
 // Método REDUCE, nos ayuda a acumular los valores de un arreglo
 
-const numeros = [3, 40, 100, 7, 50]
+const numeros = [3, 40, 100, 7, 50] // La suma es 200
 
 // Con un FOR
 let suma = 0
@@ -157,3 +168,188 @@ const sumatoria = numeros.reduce(
 )
 
 console.log(sumatoria)
+
+// OBJETOS
+
+/*
+  {
+    KEY1: VALUE1,
+    KEY2: VALUE2,
+    KEY3: VALUE3,
+  }
+*/
+
+const miObjetoVacio = {}
+
+const miObjeto = {
+  nombre: 'Victor',
+  apellido: 'Villazón',
+  colorFavorito: 'azúl',
+  'mi edad': 38,
+  coloresFavoritos: ['rosado', 'rojo', 'azúl'],
+  cursos: [
+    {
+      id: 1,
+      nombre: 'Matemática',
+      nota: 13
+    },
+    {
+      id: 2,
+      nombre: 'Algoritmos',
+      nota: 20
+    }
+  ],
+  devolverCursosAprobados: function() { // Función anónima
+    return this.cursos.filter(function (curso) {
+      return curso.nota > 13
+    })
+  }
+}
+
+console.log(miObjetoVacio)
+console.log(miObjeto)
+
+// LEER LOS CAMPOS DE UN OBJETO (notación de punto y de corchete)
+
+console.log(miObjeto.nombre) // Victor
+console.log(miObjeto.apellido) // Villazón
+console.log(miObjeto.edad) // undefined
+
+// console.log(miObjeto.mi edad) // BAD ❌ - Uncaught SyntaxError: missing ) after argument list
+console.log(miObjeto['mi edad']) // OK ✅
+
+console.log(miObjeto.coloresFavoritos)
+console.log(miObjeto.coloresFavoritos[2])
+
+console.log(miObjeto.cursos) // Array
+console.log(miObjeto.cursos[1]) // {id: 2, nombre: 'Algoritmos', nota: 20}
+console.log(miObjeto.cursos[1].nota) // 20
+console.log(miObjeto.cursos[1]['nota']) // 20
+
+console.log(miObjeto.devolverCursosAprobados) // ƒ()
+const resultadoCursosAprobados = miObjeto.devolverCursosAprobados()
+console.log(resultadoCursosAprobados) // [{ id: 2, nombre: 'Algoritmos', nota: 20 }]
+console.log(resultadoCursosAprobados[0]['nombre']) // Algoritmos
+
+// ELIMINAR PROPIEDADES DE UN OBJETO
+
+console.log(miObjeto)
+// delete miObjeto.cursos
+delete miObjeto["mi edad"]
+console.log(miObjeto)
+
+// INSERTAR UN NUEVA PROPIEDADE A UN OBJETO
+
+miObjeto.platilloFavorito = 'Ceviche de Conchas Negras'
+miObjeto['juegos favoritos'] = ['Crash Team Racing', 'Mario', 'Minecraft']
+
+console.log(miObjeto)
+
+// DESTRUCTURING DE ARREGLOS Y OBJETOS
+
+// Una forma de extraer las elementos/propiedades de un objeto o un arreglo en nuevas variables
+
+// DESTRUCTURING: OBJETOS
+
+const nombreValue = miObjeto.nombre
+const apellidoValue = miObjeto.apellido
+const colorFavoritoValue = miObjeto.colorFavorito
+
+console.log(nombreValue, apellidoValue, colorFavoritoValue)
+
+const { nombre, apellido, colorFavorito } = miObjeto
+
+console.log(nombre, apellido, colorFavorito)
+
+const {
+  nombre: nombreConUnAlias,
+  apellido: apellidoConUnAlias,
+  cursos: cursosValor
+} = miObjeto
+
+console.log(nombreConUnAlias, apellidoConUnAlias, cursosValor)
+
+// DESTRUCTURING: ARRAYS
+
+const amigos = ['leo', 'marcial', 'diego', 'yesica', 'victor']
+
+const [amigo1, amigo2, amigo3, , amigo4] = amigos
+
+console.log(amigo1, amigo2, amigo3, amigo4)
+
+const [a1, a2, ...mejoresAmigos] = amigos
+
+console.log(a1, a2, mejoresAmigos)
+
+// SPREAD OPERATOR (operador ...)
+
+// Simplifica el extraer las propiedades de un objeto/arreglo para reutilizarlo en otros objetos/arreglos.
+
+const producto = {
+  nombre: 'Laptop',
+  precio: 4890,
+  categoria: 'tech'
+}
+
+const cliente = {
+  nombre: 'Alessandro',
+  isVip: true
+}
+
+console.log(producto + cliente) // ❌ 💔 [object Object][object Object]
+
+const nuevoObjeto = { ...producto, ...cliente } // ⛔ Cuidado
+
+console.log(nuevoObjeto)
+
+// SPREAD OPERATOR, evitando colisiones de propiedades
+
+const nuevoObjetoSinColisiones = {
+  product: { ...producto },
+  cliente: { ...cliente }
+}
+
+console.log(nuevoObjetoSinColisiones)
+console.log(nuevoObjetoSinColisiones.product.nombre) // Laptop
+console.log(nuevoObjetoSinColisiones.cliente.nombre) // Alessandro
+
+// OTROS MÉTODOS DE OBJETOS
+
+console.log(Object.keys(producto)) // Obtenemos solo las claves (keys) del objeto dentro de un arreglo de cadenas.
+console.log(Object.values(producto)) // Obtenemos solo los values (values) del objeto dentro de un arreglo de cadenas.
+console.log(Object.entries(producto)) // Convertimos un objeto en un arreglo
+
+// EJEMPLO FINAL CON OBJETOS
+
+const hero = 'Victor'
+
+if (hero === 'Batman') {
+  console.log('Hola soy Bruce')
+} else if (hero === 'Spiderman') {
+  console.log('Hola soy Peter')
+} else if (hero === 'Ironman') {
+  console.log('Hola soy Tony')
+} else {
+  console.log('No soy un heroe 😒')
+}
+
+const SUPER_HEROS = {
+  'Batman': 'Hola soy Bruce',
+  'Spiderman': 'Hola soy Peter',
+  'Ironman': 'Hola soy Tony',
+}
+
+if (!SUPER_HEROS[hero]) {
+  console.log('No soy un heroe 😒')
+} else {
+  console.log(SUPER_HEROS[hero])
+}
+
+console.log(SUPER_HEROS[hero] ?? 'No soy un heroe 😉')
+
+
+// TODO: Desarrollar 1 reto del siguiente link para mañana
+// TODO: Desarrollar 3 retos más del siguiente link
+
+// https://adventjs.dev/#retos
+// https://2021.adventjs.dev/challenges
