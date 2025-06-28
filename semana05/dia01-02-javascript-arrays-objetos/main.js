85728a78320b4ff1fe46174e6aded71ad198d90f
Victor Villazón <victorzn23@gmail.com>
2025-06-28 11:56:47 -0500

    semana05, dia01, javascript arrays+objetos

--- semana05/dia01-02-javascript-arrays-objetos/main.js
+++ semana05/dia01-02-javascript-arrays-objetos/main.js
@@ -10,8 +10,15 @@
 
 const arregloVacio = []
 let listaDeNumeros = [25, 36, 89, 1560]
-const listaDeValores = [1, 2 , 3, 'Victor', 'Villazón', true, null, undefined]
+const listaDeValores = [1, 2 , 3, 'Victor', 'Villazón', true, null, undefined, [9, 6, 2]]
 
 console.log(arregloVacio)
 console.log(listaDeNumeros);
 console.log(listaDeValores)
+
+// Lectura de la elemento de un arreglo
+
+console.log(listaDeValores[0]) // 1
+console.log(listaDeValores[4]) // Villazón
+console.log(listaDeValores[5]) // true
+console.log(listaDeValores[99]) // undefined
