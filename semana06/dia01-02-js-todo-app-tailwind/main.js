49f266b81ea9498ff4ad5810605ffdac0648503f
Victor Villazón <victorzn23@gmail.com>
2025-07-05 11:39:19 -0500

    semana06, dia01, javascript todo app

--- semana06/dia01-02-js-todo-app-tailwind/main.js
+++ semana06/dia01-02-js-todo-app-tailwind/main.js
@@ -10,4 +10,26 @@ const taskList = document.querySelector('.task__list')
 taskAdd.addEventListener('click', function(event) {
   // Esta función se ejecutará cuando hagamos click en el botón 'Añadir tarea'
   console.log('Click!', taskInput.value)
+
+  // MANEJO DEL DOM: Es una forma de crear elementos dinámicamente con el DOM de JS
+  // const button = document.createElement('button')
+  // button.textContent = 'Hola soy un botón!'
+  // document.body.appendChild(button)
+
+  // TODO: 04 - Añadir el elemento checkbox al elemento li
+
+  // 01 - Añadir el elemento li al elemento con la clase task__list
+  const li = document.createElement('li')
+  // 02 - Añadir el elemento span al elemento li
+  const span = document.createElement('span')
+  span.textContent = taskInput.value
+  li.appendChild(span)
+  // 03 - Añadir el elemento button al elemento li
+  const button = document.createElement('button')
+  button.textContent = 'Borrar'
+  li.appendChild(button)
+
+  console.log(li)
+
+  taskList.appendChild(li)
 })
