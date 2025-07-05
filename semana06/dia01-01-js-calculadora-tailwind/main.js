a63ad78dd0f1bad35db0e7a2e3523790c67069e9
Victor Villazón <victorzn23@gmail.com>
2025-07-05 10:04:01 -0500

    semana06, dia01, javascript dom

--- semana06/dia01-01-js-calculadora-tailwind/main.js
+++ semana06/dia01-01-js-calculadora-tailwind/main.js
@@ -1,5 +1,29 @@
+let numeroActual = '0'
+let operador = '' // + - *
+let operando = '' // Cualquier número. Ejemplo: 9
+
 const elInputDisplay = document.querySelector('.inputDisplay')
+const elButtons = document.querySelectorAll('button') // Devuelve un array
 
 // console.log({ elInputDisplay }) // El objeto del elemento input
 // elInputDisplay.value = '999'
+// console.log({ elButtons })
 
+// for(let i = 0; i < elButtons.length; i++) {
+//   console.log(elButtons[i])
+// }
+
+elButtons.forEach(function(button, index) {
+  // console.log(index, {button})
+
+  button.addEventListener('click', function(event) {
+    // console.log('Hice click!', event, { target: event.target })
+    const buttonText = event.target.textContent
+
+    console.log(buttonText)
+
+    // TODO: Añadir la lógica de la calculadora
+
+    
+  })
+})
