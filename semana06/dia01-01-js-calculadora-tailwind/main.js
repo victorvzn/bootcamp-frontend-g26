e8c01404279570a4ede8fdcf0ce57a2954d826a1
Victor Villazón <victorzn23@gmail.com>
2025-07-05 10:23:59 -0500

    semana06, dia01, javascript dom

--- semana06/dia01-01-js-calculadora-tailwind/main.js
+++ semana06/dia01-01-js-calculadora-tailwind/main.js
@@ -20,10 +20,38 @@ elButtons.forEach(function(button, index) {
     // console.log('Hice click!', event, { target: event.target })
     const buttonText = event.target.textContent
 
-    console.log(buttonText)
+    // console.log(buttonText)
 
     // TODO: Añadir la lógica de la calculadora
 
-    
-  })
+    // 1. Evaluamos cuando hacemos click en un operador(+, -, *)
+    if ('+-*'.includes(buttonText)) {
+      console.log('OPERADOR')
+      operador = buttonText
+      operando = Number(numeroActual)
+      numeroActual = '0'
+    // 3. Evaluamos el botón '='
+    } else if (buttonText === '=') {
+      console.log('IGUAL')
+      if (operador === '+') {
+        numeroActual = Number(operando) + Number(numeroActual)
+      } else if (operador === '-') {
+        numeroActual = Number(operando) - Number(numeroActual)
+      } else if (operador === '*') {
+        numeroActual = Number(operando) * Number(numeroActual)
+      }
+    // 4. Evaluamos el botón 'CE' y limpiamos operando, operadores y el input
+    } else if (buttonText === 'CE') {
+      console.log('CE')
+      numeroActual = '0'
+      operador = ''
+      operando = ''
+    // 2. Evaluamos cuando se presiona algún número
+    } else {
+      console.log(buttonText)
+      numeroActual = Number(numeroActual + buttonText)
+    }
+
+    elInputDisplay.value = numeroActual
+   })
 })
