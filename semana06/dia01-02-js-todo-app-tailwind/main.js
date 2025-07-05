0f6a010f076277f0d6cba784b2164a731a731fc8
Victor Villazón <victorzn23@gmail.com>
2025-07-05 11:24:00 -0500

    semana06, dia01, javascript todo app

--- semana06/dia01-02-js-todo-app-tailwind/main.js
+++ semana06/dia01-02-js-todo-app-tailwind/main.js
@@ -1,1 +1,13 @@
-console.log('JS')
+const taskInput = document.getElementById('taskInput')
+const taskAdd = document.querySelector('.task__add')
+const taskList = document.querySelector('.task__list')
+
+// console.log({ taskInput, taskAdd, taskList })
+
+// EVENTOS
+// ELEMENTO.addEventListener(NOMBRE_EVENTO, CALLBACK)
+
+taskAdd.addEventListener('click', function(event) {
+  // Esta función se ejecutará cuando hagamos click en el botón 'Añadir tarea'
+  console.log('Click!', taskInput.value)
+})
