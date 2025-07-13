0c7297b27806adb8e0c4437b57c7216e7f02bc87
Victor Villazón <victorzn23@gmail.com>
2025-07-13 12:32:01 -0500

    semana08, dia01, js fetch promises ejercicio

--- semana08/dia01-02-javascript-fetch-ejercicio/main.js
+++ semana08/dia01-02-javascript-fetch-ejercicio/main.js
@@ -2,3 +2,28 @@
 
 // API: https://dragonball-api.com/api/characters
 
+const url = 'https://dragonball-api.com/api/characters'
+
+const renderCharacters = (characters=[]) => {
+  const elApp = document.querySelector('#app')
+
+  let characterList = ''
+
+  characters.forEach(character => {
+    characterList += `
+      <article>
+        <h2>${character.id} - ${character.name}</h2>
+        <img src="${character.image}" width="100" />
+      </article>
+    `
+  })
+
+  elApp.innerHTML = characterList
+}
+
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    renderCharacters(data.items)
+    console.log(data)
+  })
