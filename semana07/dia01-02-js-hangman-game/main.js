0b8f3d93fca8681bc7803e3088133f69b0049b27
Victor Villazón <victorzn23@gmail.com>
2025-07-13 10:21:34 -0500

    semana07, dia02, js hangman game

--- semana07/dia01-02-js-hangman-game/main.js
+++ semana07/dia01-02-js-hangman-game/main.js
@@ -11,6 +11,8 @@ const word = ANSWER_WORD.toUpperCase()
 
 console.log(word)
 
+let correctGuess = ''
+
 // Implementar una función para renderizar los botones con cada letra del alfabeto de la A a la Z
 
  // TODO: Reemplazar por una función que genere las letras de la A a la Z
@@ -20,14 +22,38 @@ const renderAlphabet = () => {
   let list = ''
 
   ALPHABET.forEach(letter => {
+    let statusClass = ''
+    
+    const isInCorrectGuesses = correctGuess.includes(letter)
+    const isInAnswerWord = word.includes(letter)
+
+    if (isInCorrectGuesses && isInAnswerWord) {
+      statusClass = 'bg-green-500 hover:bg-green-600'
+    } else if (isInCorrectGuesses) {
+      statusClass = 'bg-red-500 hover:bg-red-600 '
+    }
+
     list += `
-      <button class="p-2 bg-blue-500 text-white font-bold text-xl cursor-pointer hover:bg-blue-600 duration-300 rounded-md">
+      <button
+        class="p-2 bg-blue-500 text-white font-bold text-xl cursor-pointer hover:bg-blue-600 duration-300 rounded-md ${statusClass}"
+        onclick="selectLetter('${letter}')"
+      >
         ${letter}
       </button>
     `
   })
 
   elButtons.innerHTML = list
+}
+
+const selectLetter = (letter) => {
+  console.log({ letter })
+
+  correctGuess += letter
+  
+  console.log('intentos', correctGuess)
+
+  renderAlphabet()
 }
 
 renderAlphabet()
