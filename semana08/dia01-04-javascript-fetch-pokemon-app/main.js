aed55e795e3986563972ff33ee8e235abce6e4e0
Victor Villazón <victorzn23@gmail.com>
2025-07-19 10:04:19 -0500

    semana08, dia01, js fetch pokemon app

--- semana08/dia01-04-javascript-fetch-pokemon-app/main.js
+++ semana08/dia01-04-javascript-fetch-pokemon-app/main.js
@@ -1,1 +1,27 @@
-console.log('js')
+const fetchPokemons = async () => {
+  const url = 'https://pokeapi.co/api/v2/pokemon/'
+
+  const response = await fetch(url)
+
+  return response.json()
+}
+
+const renderPokemons = (pokemons = []) => {
+  const pokemonList = document.querySelector('#pokemonList')
+
+  let elements = ''
+
+  pokemons.forEach(pokemon => {
+    elements += `
+      <h2>${pokemon.name}</h2>
+    `
+  })
+
+  pokemonList.innerHTML = elements
+}
+
+fetchPokemons()
+  .then(data => {
+    console.log(data)
+    renderPokemons(data.results)
+  })
