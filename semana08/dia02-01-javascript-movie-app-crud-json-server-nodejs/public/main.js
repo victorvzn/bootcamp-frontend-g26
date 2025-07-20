458eeb51715d43c8fbb8063f6640c6b04697b052
Victor Villazón <victorzn23@gmail.com>
2025-07-20 11:45:41 -0500

    semana08, dia02, js movie app crud json server nodejs

--- semana08/dia02-01-javascript-movie-app-crud-json-server-nodejs/public/main.js
+++ semana08/dia02-01-javascript-movie-app-crud-json-server-nodejs/public/main.js
@@ -1,1 +1,52 @@
- // TODO: Crear la función renderMovies para mostrar los datos de las peliculas en la tabla.
+ // TODO: 01 - Crear la función renderMovies para mostrar los datos de las peliculas en la tabla.
+
+ const renderMovies = (movies = []) => {
+  const elMovieList = document.querySelector('.movies__list')
+
+  let elements = ''
+
+  movies.forEach(movie => {
+    elements += `
+      <tr>
+        <td>${movie.id}</td>
+        <td>
+          <img
+            src="${movie.image}
+            width="100"
+            height="150"
+          />
+        </td>
+        <td>
+          <strong>${movie.name}</strong>
+          <div class="text-small">
+            <strong>Release:</strong> ${movie.release}
+          </div>
+          <div class="text-small">
+            <strong>Summary:</strong> ${movie.summary}
+          </div>
+        </td>
+        <td>
+          <div class="flex gap-0.5">
+            <button class="movies__edit">✏</button>
+            <button class="movies__remove">❌</button>
+          </div>
+        </td>
+      </tr>
+    ` 
+  })
+
+  elMovieList.innerHTML = elements
+}
+
+const fetchMovies = async () => {
+  const url = 'http://localhost:3000/movies'
+
+  const response = await fetch(url)
+
+  return await response.json()
+}
+
+fetchMovies()
+  .then(data => {
+    renderMovies(data)
+  })
