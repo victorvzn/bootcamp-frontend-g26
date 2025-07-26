0b4ce7db2a6c6bcfa034331e2c7a2f25a0b7194f
Victor Villazón <victorzn23@gmail.com>
2025-07-26 10:04:37 -0500

    semana09, dia01, react intro

--- semana09/dia01-01-react-intro/src/App.jsx
+++ semana09/dia01-01-react-intro/src/App.jsx
@@ -1,7 +1,52 @@
-function App() {
+// 04 - EXTENSION NECESARIA PARA USAR REACT EN VSCODE
+
+
+
+const App = () => {
+  // Cuerpo de la función donde va la lógica del componente
+
   return (
-    <h1>Vite + React</h1>
+    <>
+      <h1>Hola React!</h1>
+      <p>Estamos aprendiendo más!</p>
+    </>
   )
 }
 
 export default App
+
+// 03 - COMPONENTE USANDO MULTIPLES LÍNEAS + FRAGMENTS
+
+// const App = () => {
+//   // Cuerpo de la función donde va la lógica del componente
+
+//   return (
+//     <>
+//       <h1>Hola React!</h1>
+//       <p>Estamos aprendiendo más!</p>
+//     </>
+//   )
+// }
+
+// export default App
+
+// 02 - COMPONENTE USANDO ARROW FUNTIONS
+
+// const App = () => {
+//   // Cuerpo de la función donde va la lógica del componente
+
+//   return <h1>Hola React!</h1>
+// }
+
+// export default App
+
+
+// 01 - COMPONENTE USANDO FUNCTION
+
+// function App() {
+//   return (
+//     <h1>Vite + React</h1>
+//   )
+// }
+
+// export default App
