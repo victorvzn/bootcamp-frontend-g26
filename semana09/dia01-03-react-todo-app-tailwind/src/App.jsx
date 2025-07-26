4f0e529590fe56c770d08948485d82d355d4366d
Victor Villazón <victorzn23@gmail.com>
2025-07-26 12:17:44 -0500

    semana09, dia01, react todo app tw

--- semana09/dia01-03-react-todo-app-tailwind/src/App.jsx
+++ semana09/dia01-03-react-todo-app-tailwind/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const DEFAULT_TODOS = [
+    {
+      id: '1',
+      title: 'Aprender Javascript',
+      completed: true
+    },
+    {
+      id: '2',
+      title: 'Aprender CSS',
+      completed: false
+    },
+    {
+      id: '3',
+      title: 'Aprender React.js + TailwindCSS',
+      completed: true
+    }
+  ]
+
+  const [todos, setTodos] = useState(DEFAULT_TODOS)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <header>
+        <h1>
+          Todo App
+        </h1>
+      </header>
+
+
+      <pre>{JSON.stringify(todos, null, 2)}</pre>
+    </main>
   )
 }
-
-export default App
