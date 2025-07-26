6e9eb9ba6cb3dc875dbf10c5f4b6c7ba26703c35
Victor Villazón <victorzn23@gmail.com>
2025-07-26 11:51:46 -0500

    semana09, dia01, react calculadora tw

--- semana09/dia01-02-react-calculadora-tailwindcss/src/App.jsx
+++ semana09/dia01-02-react-calculadora-tailwindcss/src/App.jsx
@@ -1,4 +1,40 @@
+import { useState } from "react"
+
 export default function App() {
+  const [numeroActual, setNumeroActual] = useState('0')
+  const [operador, setOperador] = useState('')
+  const [operando, setOperando] = useState('')
+
+  // [valor, funcion]
+
+  const handleButton = (event) => {
+    const buttonText = event.target.textContent
+    
+    console.log('hice click', buttonText)
+
+    // 1. Evaluamos cuando hacemos click en un operador(+, -, *)
+    if ('+-*'.includes(buttonText)) {
+      setOperador(buttonText)
+      setOperando(Number(numeroActual))
+      setNumeroActual('0')
+    } else if (buttonText === '=') {
+      if (operador === '+') {
+        setNumeroActual(Number(operando) + Number(numeroActual))
+      } else if (operador === '-') {
+        setNumeroActual(Number(operando) - Number(numeroActual))
+      } else if (operador === '*') {
+        setNumeroActual(Number(operando) * Number(numeroActual))
+      }
+    } else if (buttonText === 'CE') {
+      setNumeroActual('0')
+      setOperador('')
+      setOperando('')
+    } else {
+      // 2. Evaluamos cuando se presiona algún número
+      setNumeroActual(Number(numeroActual + buttonText))
+    }
+  }
+
   return (
     <main className="bg-blue-400 w-[340px] my-0 mx-auto mt-5 p-4 rounded-md shadow-lg">
       <h1 className="text-3xl font-bold text-center mb-4">
@@ -6,59 +42,59 @@ export default function App() {
       </h1>
 
       <section className="flex flex-col gap-2">
-        <input
+        <input  
           type="text"
           className="border p-2 text-right text-2xl font-bold rounded-md"
-          value="0"
+          value={numeroActual}
           readOnly
         />
 
         <div className="grid grid-cols-3 gap-3">
-          <button className="button bg-red-400 p-2 rounded-md hover:bg-red-500 duration-300">
+          <button onClick={handleButton} className="button bg-red-400 p-2 rounded-md hover:bg-red-500 duration-300">
             +
           </button>
-          <button className="button bg-red-400 p-2 rounded-md hover:bg-red-500 duration-300">
+          <button onClick={handleButton} className="button bg-red-400 p-2 rounded-md hover:bg-red-500 duration-300">
             -
           </button>
-          <button className="button bg-red-400 p-2 rounded-md hover:bg-red-500 duration-300">
+          <button onClick={handleButton} className="button bg-red-400 p-2 rounded-md hover:bg-red-500 duration-300">
             *
           </button>
 
-          <button className="button bg-yellow-400 p-2 rounded-md hover:bg-yellow-500 duration-300">
+          <button onClick={handleButton} className="button bg-yellow-400 p-2 rounded-md hover:bg-yellow-500 duration-300">
             7
           </button>
-          <button className="button bg-yellow-400 p-2 rounded-md hover:bg-yellow-500 duration-300">
+          <button onClick={handleButton} className="button bg-yellow-400 p-2 rounded-md hover:bg-yellow-500 duration-300">
             8
           </button>
-          <button className="button bg-yellow-400 p-2 rounded-md hover:bg-yellow-500 duration-300">
+          <button onClick={handleButton} className="button bg-yellow-400 p-2 rounded-md hover:bg-yellow-500 duration-300">
             9
           </button>
-          <button className="button bg-yellow-400 p-2 rounded-md hover:bg-yellow-500 duration-300">
+          <button onClick={handleButton} className="button bg-yellow-400 p-2 rounded-md hover:bg-yellow-500 duration-300">
             4
           </button>
-          <button className="button bg-yellow-400 p-2 rounded-md hover:bg-yellow-500 duration-300">
+          <button onClick={handleButton} className="button bg-yellow-400 p-2 rounded-md hover:bg-yellow-500 duration-300">
             5
           </button>
-          <button className="button bg-yellow-400 p-2 rounded-md hover:bg-yellow-500 duration-300">
+          <button onClick={handleButton} className="button bg-yellow-400 p-2 rounded-md hover:bg-yellow-500 duration-300">
             6
           </button>
-          <button className="button bg-yellow-400 p-2 rounded-md hover:bg-yellow-500 duration-300">
+          <button onClick={handleButton} className="button bg-yellow-400 p-2 rounded-md hover:bg-yellow-500 duration-300">
             1
           </button>
-          <button className="button bg-yellow-400 p-2 rounded-md hover:bg-yellow-500 duration-300">
+          <button onClick={handleButton} className="button bg-yellow-400 p-2 rounded-md hover:bg-yellow-500 duration-300">
             2
           </button>
-          <button className="button bg-yellow-400 p-2 rounded-md hover:bg-yellow-500 duration-300">
+          <button onClick={handleButton} className="button bg-yellow-400 p-2 rounded-md hover:bg-yellow-500 duration-300">
             3
           </button>
-          <button className="button bg-yellow-400 p-2 rounded-md hover:bg-yellow-500 duration-300">
+          <button onClick={handleButton} className="button bg-yellow-400 p-2 rounded-md hover:bg-yellow-500 duration-300">
             0
           </button>
 
-          <button className="button bg-green-400 p-2 rounded-md hover:bg-green-500 duration-300">
-            AC
+          <button onClick={handleButton} className="button bg-green-400 p-2 rounded-md hover:bg-green-500 duration-300">
+            CE
           </button>
-          <button className="button bg-green-400 p-2 rounded-md hover:bg-green-500 duration-300">
+          <button onClick={handleButton} className="button bg-green-400 p-2 rounded-md hover:bg-green-500 duration-300">
             =
           </button>
         </div>
