fdce7fcc590086fc833786d27dc351a0495086e3
Victor Villazón <victorzn23@gmail.com>
2025-07-26 11:08:22 -0500

    semana09, dia01, react calculadora tw

--- semana09/dia01-02-react-calculadora-tailwindcss/src/App.jsx
+++ semana09/dia01-02-react-calculadora-tailwindcss/src/App.jsx
@@ -1,5 +1,65 @@
 export default function App() {
   return (
-    <div className="bg-blue-400">App</div>
+    <main className="bg-blue-400">
+      <h1>
+        Calculador + React.js
+      </h1>
+
+      <section>
+        <input
+          type="text"
+        />
+
+        <div>
+          <button>
+            +
+          </button>
+          <button>
+            -
+          </button>
+          <button>
+            *
+          </button>
+
+          <button>
+            7
+          </button>
+          <button>
+            8
+          </button>
+          <button>
+            9
+          </button>
+          <button>
+            4
+          </button>
+          <button>
+            5
+          </button>
+          <button>
+            6
+          </button>
+          <button>
+            1
+          </button>
+          <button>
+            2
+          </button>
+          <button>
+            3
+          </button>
+          <button>
+            *
+          </button>
+
+          <button>
+            AC
+          </button>
+          <button>
+            =
+          </button>
+        </div>
+      </section>
+    </main>
   )
 }
