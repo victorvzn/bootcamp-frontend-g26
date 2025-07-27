4cbc1eb7d38c608b3c7308b4cba13d87cb9f0ba5
Victor Villazón <victorzn23@gmail.com>
2025-07-27 09:30:23 -0500

    semana09, dia02, react todo app tw

--- semana09/dia01-03-react-todo-app-tailwind/src/App.jsx
+++ semana09/dia01-03-react-todo-app-tailwind/src/App.jsx
@@ -23,11 +23,26 @@ export default function App() {
 
   const [todos, setTodos] = useState(DEFAULT_TODOS)
 
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    console.log('Agregando tareas...')
+  }
+
   return (
     <main
       className="bg-yellow-100 w-[400px] mx-auto mt-10 border border-yellow-400 rounded-lg shadow-md p-4"
     >
       <TodoHeader title='Todo App + React + Tailwind' />
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="¿Qué deseas hacer hoy?"
+          required
+          className="border w-full border-yellow-400 my-3 px-2 py-3 rounded-lg"
+        />
+      </form>
 
       <TodoList todos={todos} />
 
