70f8b817dfbfccca255f13aa91df7be417b0db83
Victor Villazón <victorzn23@gmail.com>
2025-07-27 11:50:41 -0500

    semana09, dia02, react todo app tw

--- semana09/dia01-03-react-todo-app-tailwind/src/components/TodoList.jsx
+++ semana09/dia01-03-react-todo-app-tailwind/src/components/TodoList.jsx
@@ -2,6 +2,8 @@ export const TodoList = ({ todos, onRemoveTodo, onCompleted }) => {
   // TODO: Agregar un checkbox en cada fila y cambiar el estado de cada todo en su key completed
   // TODO: Y además debemos tachar el texto si la tarea está completada
 
+  // TODO: Editar el título de la tarea haciendo doble click en el título, esto hará que aparezca una caja de texto. Y para guardarlo solo debemos presionar enter en la caja de texto y actualizamos el estados todos.
+
   return (
     <ul className="flex flex-col gap-2 mt-4">
       {todos.map(todo => {
@@ -17,7 +19,7 @@ export const TodoList = ({ todos, onRemoveTodo, onCompleted }) => {
             />
             <div className="w-full flex justify-between items-center gap-2">
               <span
-                className="font-medium w-full p-1"
+                className={`font-medium w-full p-1 ${todo.completed ? 'line-through' : '' }`}
               >
                 {todo.title}
               </span>
