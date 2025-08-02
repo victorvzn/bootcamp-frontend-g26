bd9da576a48975576a9fb55bd70d011da0a0a630
Victor Villazón <victorzn23@gmail.com>
2025-08-02 11:13:32 -0500

    semana09, dia02, react todo app tw

--- semana09/dia02-01-react-student-list-app-tailwindcss/src/services/students.js
+++ semana09/dia02-01-react-student-list-app-tailwindcss/src/services/students.js
@@ -3,7 +3,21 @@ const MOCKAPI_URL = 'https://68865f31f52d34140f6c1285.mockapi.io/api/v1/'
 const ENDPOINT_STUDENTS = `${MOCKAPI_URL}/students`
 
 export const fetchStudents = async () => { // Devuelve una promesa
-  const response = await fetch(ENDPOINT_STUDENTS)
+  const response = await fetch(ENDPOINT_STUDENTS) // GET
 
   return await response.json()
+}
+
+export const createStudent = async (data) => {
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  }
+
+  const reponse = await fetch(ENDPOINT_STUDENTS, options)
+
+  return await reponse.json()
 }
