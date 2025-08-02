699cffedab4fc1c4d1f17d3cd9d336d502167969
Victor Villazón <victorzn23@gmail.com>
2025-08-02 10:12:01 -0500

    semana09, dia02, react student list app tw

--- semana09/dia02-01-react-student-list-app-tailwindcss/src/App.jsx
+++ semana09/dia02-01-react-student-list-app-tailwindcss/src/App.jsx
@@ -15,7 +15,33 @@ export default function App() {
 
       <h1 className="text-2xl font-semibold text-center mb-3">Student list - CRUD</h1>
     
-      <form></form>
+      <form className="flex flex-col gap-3 bg-slate-300 p-3 rounded-lg shadow-lg">
+        <label className="flex flex-col gap-2">
+          <span className="text-sm font-medium text-slate-700">Name</span>
+          <input
+            className="bg-slate-50 border border-slate-300 text-slate-700 text-sm rounded-lg py-2.5 px-2"
+            type="text"
+            name="name"
+            placeholder="Ex. Victor Villazón"
+            required
+          />
+        </label>
+        <label className="flex flex-col gap-2">
+          <span className="text-sm font-medium text-slate-700">City</span>
+          <input
+            className="bg-slate-50 border border-slate-300 text-slate-700 text-sm rounded-lg py-2.5 px-2"
+            type="text"
+            name="city"
+            placeholder="Ex. Chiclayo"
+            required
+          />
+        </label>
+        <input
+          className=" bg-blue-800 text-white hover:bg-blue-900 font-medium rounded-lg text-sm w-full py-2.5 cursor-pointer"
+          type="button"
+          value="Save student"
+        />
+      </form>
 
       <pre  className="mt-8 border border-slate-300 p-3 rounded-lg bg-slate-50 overflow-y-auto">{JSON.stringify(students, null, 2)}</pre>
     </main>
