afda88ac10a5c946defd0bdcc4aa6c23320f9be2
Victor Villazón <victorzn23@gmail.com>
2025-08-03 09:46:21 -0500

    semana10, dia02, react pokemon list fetch api useeffect + tailwind

--- semana10/dia02-01-react-pokemon-list-fetch-api-useeffect/src/App.jsx
+++ semana10/dia02-01-react-pokemon-list-fetch-api-useeffect/src/App.jsx
@@ -1,6 +1,44 @@
+import { useEffect, useState } from "react"
+
 const App = () => {
+  const [pokemons, setPokemons] = useState([])
+
+  // Genera un llamado infinito al api de pokemons por que actualizamos el estado pokemons
+  // fetch('https://pokeapi.co/api/v2/pokemon') // Devuelve una promise
+  //   .then(response => response.json())
+  //   .then(data => {
+  //     setPokemons(data.results)
+  //   })
+
+  // useEffect: Manejar el ciclo de vida de nuestro componente: Creación, actualización, destrucción
+  // useEffect(fn, [dependencies])
+  // Un arreglo en las dependencias significa que se ejecuta solo en el primer render
+
+  useEffect(() => {
+      fetch('https://pokeapi.co/api/v2/pokemon') // Devuelve una promise
+        .then(response => response.json())
+        .then(data => {
+          console.log(data)
+          setPokemons(data.results)
+        })
+  }, [])
+
   return (
-    <div>App</div>
+    <>
+      <section>
+        <h1 className="text-2xl text-center">Pokemon List</h1>
+
+        <ul>
+          {pokemons.map((pokemon, index) => {
+            return (
+              <li key={pokemon.url}>{index}: {pokemon.name}</li>
+            )
+          })}
+        </ul> 
+      </section>
+
+      <pre className="bg-slate-200 p-4">{JSON.stringify(pokemons, null, 2)}</pre>
+    </>
   )
 }
 
