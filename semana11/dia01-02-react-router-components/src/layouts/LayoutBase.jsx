52c6f30b259c704c5ede143cc0f9d3fa274b14e7
Victor Villazón <victorzn23@gmail.com>
2025-08-10 09:36:28 -0500

    semana11, dia02, react router components + tailwind

--- semana11/dia01-02-react-router-components/src/layouts/LayoutBase.jsx
+++ semana11/dia01-02-react-router-components/src/layouts/LayoutBase.jsx
@@ -1,11 +1,22 @@
-import { Outlet } from "react-router"
+import { Link, Outlet } from "react-router"
 
 const LayoutBase = () => {
   return (
     <>
-      <div>LayoutBase (ESTATICO)</div>
+      <header className="bg-yellow-300 p-4">
+        <div className="container mx-auto flex justify-between">
+          <h1 className="font-bold">React router</h1>
 
-      <Outlet />
+          <nav className="flex gap-4">
+            <Link to='/'>Home</Link>
+            <Link to='/login'>Logout</Link>
+          </nav>
+        </div>
+      </header> 
+
+      <main>
+        <Outlet />
+      </main>
     </>
   )
 }
