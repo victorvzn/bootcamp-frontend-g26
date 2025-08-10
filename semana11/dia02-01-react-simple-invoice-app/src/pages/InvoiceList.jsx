4b3e281cc985bea664181c525f84c8f924e50bfa
Victor Villazón <victorzn23@gmail.com>
2025-08-10 11:38:42 -0500

    semana11, dia02, react router components + tailwind

--- semana11/dia02-01-react-simple-invoice-app/src/pages/InvoiceList.jsx
+++ semana11/dia02-01-react-simple-invoice-app/src/pages/InvoiceList.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 
 import Header from "../components/invoice-list-page/Header"
+import { Link } from "react-router"
 
 const InvoiceList = () => {
   const [invoices, setInvoices] = useState([])
@@ -13,9 +14,45 @@ const InvoiceList = () => {
 
   return (
     <>
-      <Header title='Invoices' />
+      <Header title='Invoices' invoices={invoices.length} />
 
-      <pre>{JSON.stringify(invoices, null, 2)}</pre>
+      <section
+        className="md:w-[740px] px-2 mx-auto flex flex-col gap-5 "
+      >
+        {invoices.map(invoice => {
+          return (
+            <article
+              key={invoice.id}
+              className="border rounded px-4 py-4 text-base shadow md:flex grid grid-col-2 justify-between items-center gap-3"
+            >
+              <div>
+                #{invoice.code}
+              </div>
+              <div>
+                {invoice.date}
+              </div>
+              <div>
+                {invoice.fullName}
+              </div>
+              <div>
+                {invoice.currencySymbol}
+                {invoice.total}
+              </div>
+              <div>
+                {invoice.status}
+              </div>
+              <div>
+                <Link to={`/invoices/${invoice.id}`}>
+                  ↗
+                </Link>
+              </div>
+            </article>
+          )
+        })}
+
+      </section>
+
+      <pre className="mt-8 border p-4">{JSON.stringify(invoices, null, 2)}</pre>
     </>
   )
 }
