00231ad95ea8d41cbf0e367d4f15a12ae6b7f666
Victor Villazón <victorzn23@gmail.com>
2025-08-10 10:57:59 -0500

    semana11, dia02, react router components + tailwind

--- semana11/dia02-01-react-simple-invoice-app/src/pages/InvoiceList.jsx
+++ semana11/dia02-01-react-simple-invoice-app/src/pages/InvoiceList.jsx
@@ -1,21 +1,10 @@
-import { Link } from "react-router"
+import Header from "../components/invoice-list-page/Header"
 
 const InvoiceList = () => {
   return (
-    <header className="md:w-[740px] mx-auto flex justify-between items-center py-4 mb-4">
-      <div className="text-zinc-800">
-        <h1 className="text-4xl font-extrabold">
-          Invoices
-        </h1>
-        <span>There are <strong>0</strong> total invoices</span>
-      </div>
+    <Header title='Invoices' />
 
-      <Link to='/invoices/new'>
-        <button className="bg-indigo-400 hover:bg-indigo-500 duration-300 rounded-full px-5 py-4 font-semibold min-w-[100px] text-white cursor-pointer">
-          New invoice
-        </button>
-      </Link>
-    </header>
+    
   )
 }
 
