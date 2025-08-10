0013f7251fcf2d5621aee8184b01003cd6ab2cfc
Victor Villazón <victorzn23@gmail.com>
2025-08-10 11:17:39 -0500

    semana11, dia02, react router components + tailwind

--- semana11/dia02-01-react-simple-invoice-app/src/pages/InvoiceList.jsx
+++ semana11/dia02-01-react-simple-invoice-app/src/pages/InvoiceList.jsx
@@ -1,10 +1,22 @@
+import { useEffect, useState } from "react"
+
 import Header from "../components/invoice-list-page/Header"
 
 const InvoiceList = () => {
-  return (
-    <Header title='Invoices' />
+  const [invoices, setInvoices] = useState([])
 
-    
+  useEffect(() => {
+    fetch('http://localhost:5173/v1/invoices.json')
+      .then(response => response.json())
+      .then(data => setInvoices(data))
+  }, [])
+
+  return (
+    <>
+      <Header title='Invoices' />
+
+      <pre>{JSON.stringify(invoices, null, 2)}</pre>
+    </>
   )
 }
 
